5ef41a6b25563ade566acfea8b69d8e2604cf001
Karthik <bkp.karthi@gmail.com>
2021-01-04 19:51:38 +0530

    added input and deletion of files

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import time
 
 x = 0
 
+ask = int(input('how many times do you want to spam: '))
+
 time.sleep(1)
 
-while True:
-    x += 1
+for i in range(ask + 1):
     pg.typewrite('mkdir ' + str(x) + '\n')
+    x += 1
+
+time.sleep(1)
+
+for i in range(ask + 2):
+    pg.typewrite('rm -d ' + str(x) + '\n')
+    x -= 1
